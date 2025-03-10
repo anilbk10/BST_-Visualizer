9e82310af47170f9c23c710bd57bb48309a645b8
anil bk <abk33916@gmail.com>
2025-03-10 20:32:16 +0545

    All files are added

--- js/nodes.js
+++ js/nodes.js
@@ -1,364 +1,353 @@
 const circle = "#FFFDD0";
-const highlightedCircle = "lightblue";
 const circleText = "black";
-const highlightCircleText = "white";
 const radius = 20;
 const width = 1300;
 const height = 1000;
-//const xOffset = 100; // Horizontal spacing between nodes
-const yOffset = 100; // Vertical spacing between levels;
+const yOffset = 100;
 
 // Create SVG container
-const svg = d3.select("#treeContainer")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+const svg = d3
+  .select("#treeContainer")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
 let root = null;
 
 class Node {
-    constructor(value, x, y) {
-        this.value = value;
-        this.x = x; 
-        this.y = y;
-        this.left = null;
-        this.right = null;
-        this.fill = circle;
-        this.highlighted = false;
-    }
+  constructor(value, x, y) {
+    this.value = value;
+    this.x = x;
+    this.y = y;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.fill = circle;
+  }
 }
 
-// Function to get input value
 function getInputValue() {
-    const input = document.getElementById('inputValue').value;
-    document.getElementById('inputValue').value = ''; // Clear input after reading
-    return parseInt(input);
+  const input = document.getElementById("inputValue").value;
+  document.getElementById("inputValue").value = "";
+  return parseInt(input);
 }
 
 function calculateOffset(depth) {
-    return width / Math.pow(2, depth + 1); // Adjust spacing dynamically and calculates the number of potential nodes at the next level
+  return width / Math.pow(2, depth + 1);
 }
 
-// Insert a node into the BST with dynamic spacing
 function insertNodeHelper(node, value, depth) {
-    let offset = calculateOffset(depth);
-
-    if (value < node.value) {
-        if (node.left === null) {
-            node.left = new Node(value, node.x - offset, node.y + yOffset);  // Insert at left
-        } else {
-            insertNodeHelper(node.left, value, depth + 1);
-        }
-    } else if (value > node.value) {
-        if (node.right === null) {
-            node.right = new Node(value, node.x + offset, node.y + yOffset);  // Insert at right
-        } else {
-            insertNodeHelper(node.right, value, depth + 1);
-        }
+  let offset = calculateOffset(depth);
+  if (value < node.value) {
+    if (node.left === null) {
+      node.left = new Node(value, node.x - offset, node.y + yOffset);
+      node.left.parent = node;
+    } else {
+      insertNodeHelper(node.left, value, depth + 1);
     }
+  } else if (value > node.value) {
+    if (node.right === null) {
+      node.right = new Node(value, node.x + offset, node.y + yOffset);
+      node.right.parent = node;
+    } else {
+      insertNodeHelper(node.right, value, depth + 1);
+    }
+  }
 }
-
 
 function updateNodePositions(node, depth, x, xOffset) {
-    if (node) {
-        node.x = x;
-        node.y = 30 + depth * yOffset;
-
-        // Recursively update positions of left and right children
-        updateNodePositions(node.left, depth + 1, x - xOffset / 2, xOffset / 2);
-        updateNodePositions(node.right, depth + 1, x + xOffset / 2, xOffset / 2);
-    }
+  if (node) {
+    node.x = x;
+    node.y = 30 + depth * yOffset;
+    updateNodePositions(node.left, depth + 1, x - xOffset / 2, xOffset / 2);
+    updateNodePositions(node.right, depth + 1, x + xOffset / 2, xOffset / 2);
+  }
 }
 
 function updateVisualization() {
-    svg.selectAll("*").remove(); // Clear previous drawing
-
-    if (root) {
-        updateNodePositions(root, 0, width / 2, width / 4); // Recalculate positions
-    }
-
-    function drawNode(node) {
-        if (node) {
-            // Draw lines first to maintain layer order
-            if (node.left) {
-                svg.append("line")
-                    .attr("x1", node.x)
-                    .attr("y1", node.y)
-                    .attr("x2", node.left.x)
-                    .attr("y2", node.left.y)
-                    .attr("stroke", "black");
-                drawNode(node.left);
-            }
-            if (node.right) {
-                svg.append("line")
-                    .attr("x1", node.x)
-                    .attr("y1", node.y)
-                    .attr("x2", node.right.x)
-                    .attr("y2", node.right.y)
-                    .attr("stroke", "black");
-                drawNode(node.right);
-            }
-
-            // Draw node circle
-            svg.append("circle")
-                .attr("cx", node.x)
-                .attr("cy", node.y)
-                .attr("r", radius)
-                .attr("fill", circle)
-                .attr("stroke", "black");
-
-            //  node value
-            svg.append("text")
-                .attr("x", node.x)
-                .attr("y", node.y)
-                .attr("dy", ".35em")
-                .attr("text-anchor", "middle")
-                .style("fill", circleText)
-                .style("font-family", "sans-serif")
-                .style("font-size", "1em")
-                .text(node.value);
-        }
-    }
-
+  svg.selectAll("*").remove();
+  if (root) {
+    updateNodePositions(root, 0, width / 2, width / 4);
     drawNode(root);
+  }
 }
 
-// Insert node 
+function drawNode(node) {
+  if (node) {
+    if (node.left) {
+      svg
+        .append("line")
+        .attr("class", `line-${node.value}-${node.left.value}`)
+        .attr("x1", node.x)
+        .attr("y1", node.y)
+        .attr("x2", node.left.x)
+        .attr("y2", node.left.y)
+        .attr("stroke", "black");
+      drawNode(node.left);
+    }
+    if (node.right) {
+      svg
+        .append("line")
+        .attr("class", `line-${node.value}-${node.right.value}`)
+        .attr("x1", node.x)
+        .attr("y1", node.y)
+        .attr("x2", node.right.x)
+        .attr("y2", node.right.y)
+        .attr("stroke", "black");
+      drawNode(node.right);
+    }
+
+    let g = svg
+      .append("g")
+      .datum(node)
+      .attr("class", `node-${node.value}`) // Add class for easier selection
+      .attr("transform", `translate(${node.x}, ${node.y})`);
+
+    g.append("circle")
+      .attr("r", radius)
+      .attr("fill", node.fill)
+      .attr("stroke", "black");
+
+    g.append("text")
+      .attr("dy", ".35em")
+      .attr("text-anchor", "middle")
+      .style("fill", circleText)
+      .style("font-family", "sans-serif")
+      .style("font-size", "1em")
+      .text(node.value);
+
+    g.call(d3.drag().on("drag", dragged));
+  }
+}
+
+function dragged(event, d) {
+  d.x = event.x;
+  d.y = event.y;
+  d3.select(this).attr("transform", `translate(${d.x}, ${d.y})`);
+  if (d.parent) {
+    svg
+      .select(`.line-${d.parent.value}-${d.value}`)
+      .attr("x2", d.x)
+      .attr("y2", d.y);
+  }
+  if (d.left) {
+    svg
+      .select(`.line-${d.value}-${d.left.value}`)
+      .attr("x1", d.x)
+      .attr("y1", d.y);
+  }
+  if (d.right) {
+    svg
+      .select(`.line-${d.value}-${d.right.value}`)
+      .attr("x1", d.x)
+      .attr("y1", d.y);
+  }
+}
+
 function insertNode() {
-    const value = getInputValue();
-    if (isNaN(value)) {
-        alert("Please enter a valid number");
-        return;
-    }
-    const nodeToSearch = findNode(root, value);
-    if (nodeToSearch) {
-        
-        flickerNode(nodeToSearch, () => {
-            setTimeout(() => {
-                alert(`Node ${value} is already present`);
-            }, 200);
-        });
-    } 
-    if (root === null) {
-        root = new Node(value, width / 2, 30);
-    } 
-    else {
-        insertNodeHelper(root, value, 1);
-    }
-    updateVisualization();
+  const value = getInputValue();
+  if (isNaN(value)) {
+    alert("Please enter a valid number");
+    return;
+  }
+  if (findNode(root, value)) {
+    alert(`Node ${value} is already present`);
+    return;
+  }
+  if (root === null) {
+    root = new Node(value, width / 2, 30);
+  } else {
+    insertNodeHelper(root, value, 1);
+  }
+  updateVisualization();
 }
 
-// Clear the tree
 function clearTree() {
-    root = null;
-    updateVisualization();
+  root = null;
+  updateVisualization();
 }
 
-// Find the inorder successor
 function findMin(node) {
-    let current = node;
-    while (current && current.left !== null) {
-        current = current.left;
-    }
-    return current;
+  let current = node;
+  while (current && current.left !== null) {
+    current = current.left;
+  }
+  return current;
 }
 
-// Delete a node from the BST
 function deleteNodeHelper(node, value) {
-    if (node === null) {
-        return null;
-    }
-
-    if (value < node.value) {
-        node.left = deleteNodeHelper(node.left, value);
-    } else if (value > node.value) {
-        node.right = deleteNodeHelper(node.right, value);
-    } else {
-        // Case 1: No children
-        if (node.left === null && node.right === null) {
-            node = null;
-            return null;
-        }
-        // Case 2: One child (left only)
-        else if (node.right === null) {
-            let temp = node.left;
-            node = null;
-            return temp;
-        }
-        // Case 2: One child (right only)
-        else if (node.left === null) {
-            let temp = node.right;
-            node = null;
-            return temp;
-        }
-        // Case 3: Two children
-        else {
-            let successor = findMin(node.right);
-            node.value = successor.value;
-            node.right = deleteNodeHelper(node.right, successor.value);
-        }
-    }
-    return node;
+  if (node === null) return null;
+  if (value < node.value) {
+    node.left = deleteNodeHelper(node.left, value);
+  } else if (value > node.value) {
+    node.right = deleteNodeHelper(node.right, value);
+  } else {
+    if (node.left === null) return node.right;
+    if (node.right === null) return node.left;
+    let successor = findMin(node.right);
+    node.value = successor.value;
+    node.right = deleteNodeHelper(node.right, successor.value);
+  }
+  return node;
 }
 
-// function to find a node by value
 function findNode(node, value) {
-    if (!node) return null;  
-    if (node.value === value) return node; 
-    return findNode(node.left, value) || findNode(node.right, value);
+  if (!node) return null;
+  if (node.value === value) return node;
+  return findNode(node.left, value) || findNode(node.right, value);
 }
 
-function flickerNode(node, callback) {
-    d3.select(`circle[cx='${node.x}'][cy='${node.y}']`)
-        .transition()
-        .duration(300)
-        .attr("fill", "#90EE90") // Light green
-        .transition()
-        .duration(300)
-        .attr("fill", circle)
-        .on("end", callback); // Execute callback when animation ends
+function flickerNode(node) {
+  // Select using the node class instead of transform to handle drag changes
+  const circle = svg.select(`.node-${node.value}`).select("circle");
+  if (circle.empty()) {
+    console.error(`Circle for node ${node.value} not found`);
+    return;
+  }
+  console.log(`Flickering node ${node.value}`);
+  circle
+    .transition()
+    .duration(300)
+    .attr("fill", "#90EE90") // Green highlight
+    .transition()
+    .duration(300)
+    .attr("fill", circle.attr("fill") || circle) // Revert to initial fill
+    .on("end", () =>
+      console.log(`Transition completed for node ${node.value}`)
+    );
 }
 
-// Delete node from user input 
 function deleteNode() {
-    const value = getInputValue();
-    
-    if (isNaN(value)) {
-        alert("Please enter a valid number to delete");
-        return;
-    }
-    if (root === null) {
-        alert("Tree is empty, nothing to delete");
-        return;
-    }
-
-    
-
-    const nodeToDelete = findNode(root, value);
-    if (!nodeToDelete) {
-        alert("Node not found");
-        return;
-    }
-
-    flickerNode(nodeToDelete);
-    setTimeout(() => {
-        root = deleteNodeHelper(root, value);
-        updateVisualization();
-    }, 1000); 
-    
+  const value = getInputValue();
+  if (isNaN(value)) {
+    alert("Please enter a valid number to delete");
+    return;
+  }
+  if (root === null) {
+    alert("Tree is empty, nothing to delete");
+    return;
+  }
+  const nodeToDelete = findNode(root, value);
+  if (!nodeToDelete) {
+    alert("Node not found");
+    return;
+  }
+  flickerNode(nodeToDelete);
+  setTimeout(() => {
+    root = deleteNodeHelper(root, value);
+    updateVisualization();
+  }, 600);
 }
 
-// Search node function with flickering effect
 function searchNode() {
-    const value = getInputValue();
-    if (isNaN(value)) {
-        alert("Please enter a valid number to search");
-        return;
-    }
-    if (root === null) {
-        alert("Tree is empty, nothing to search");
-        return;
-    }
-    const nodeToSearch = findNode(root, value);
-    if (nodeToSearch) {
-        
-        flickerNode(nodeToSearch, () => {
-            setTimeout(() => {
-                alert(`Node ${value} found!`);
-            }, 500);
-        });
-    } else {
-        alert(`Node ${value} not found!`);
-    }
+  const value = getInputValue();
+  if (isNaN(value)) {
+    alert("Please enter a valid number to search");
+    return;
+  }
+  if (root === null) {
+    alert("Tree is empty, nothing to search");
+    return;
+  }
+  const nodeToSearch = findNode(root, value);
+  if (nodeToSearch) {
+    flickerNode(nodeToSearch);
+    setTimeout(() => alert(`Node ${value} found!`), 600);
+  } else {
+    alert(`Node ${value} not found!`);
+  }
 }
 
-
-function animateTraversal(nodes) {
-    let count = 0;
-    let len =nodes.length;
-    
-    function highlightThis() {
-        if (count <len ) {
-            let node = nodes[count];
-            
-            flickerNode(node, () => {
-                count++;
-                setTimeout(highlightThis, 200); // wait for next node 
-            });
-        } else {
-            alert("Traversal Order: " + nodes.map(n => n.value).join(" → "));
-        }
-    }
-
-    highlightThis();
-}
-
-// In-order Traversal (Left, Root, Right)
 function inOrderTraversalHelper(node, nodes) {
-    if (node) {
-        inOrderTraversalHelper(node.left, nodes);
-        nodes.push(node);
-        inOrderTraversalHelper(node.right, nodes);
-    }
+  if (node) {
+    inOrderTraversalHelper(node.left, nodes);
+    nodes.push(node);
+    inOrderTraversalHelper(node.right, nodes);
+  }
 }
 
 function inOrderTraversal() {
-    let nodes = [];
-    inOrderTraversalHelper(root, nodes); //populates nodes
-    animateTraversal(nodes);
+  if (!root) {
+    alert("Tree is empty");
+    return;
+  }
+  const nodes = [];
+  inOrderTraversalHelper(root, nodes);
+  animateTraversal(nodes);
 }
 
-// Pre-order Traversal (Root, Left, Right)
 function preOrderTraversalHelper(node, nodes) {
-    if (node) {
-        nodes.push(node);
-        preOrderTraversalHelper(node.left, nodes);
-        preOrderTraversalHelper(node.right, nodes);
-    }
+  if (node) {
+    nodes.push(node);
+    preOrderTraversalHelper(node.left, nodes);
+    preOrderTraversalHelper(node.right, nodes);
+  }
 }
 
 function preOrderTraversal() {
-    let nodes = [];
-    preOrderTraversalHelper(root, nodes);
-    animateTraversal(nodes);
+  if (!root) {
+    alert("Tree is empty");
+    return;
+  }
+  const nodes = [];
+  preOrderTraversalHelper(root, nodes);
+  animateTraversal(nodes);
 }
 
-// Post-order Traversal (Left, Right, Root)
 function postOrderTraversalHelper(node, nodes) {
-    if (node) {
-        postOrderTraversalHelper(node.left, nodes);
-        postOrderTraversalHelper(node.right, nodes);
-        nodes.push(node);
-    }
+  if (node) {
+    postOrderTraversalHelper(node.left, nodes);
+    postOrderTraversalHelper(node.right, nodes);
+    nodes.push(node);
+  }
 }
 
 function postOrderTraversal() {
-    let nodes = [];
-    postOrderTraversalHelper(root, nodes);
-    animateTraversal(nodes);
+  if (!root) {
+    alert("Tree is empty");
+    return;
+  }
+  const nodes = [];
+  postOrderTraversalHelper(root, nodes);
+  animateTraversal(nodes);
 }
 
-// Level-order Traversal 
 function levelOrderTraversal() {
-    let queue = [root];
-    let nodes = [];
-
-    while (queue.length > 0) {
-        let node = queue.shift();
-        nodes.push(node);
-        if (node.left) queue.push(node.left);
-        if (node.right) queue.push(node.right);
-    }
-    animateTraversal(nodes);
+  if (!root) {
+    alert("Tree is empty");
+    return;
+  }
+  const nodes = [];
+  const queue = [root];
+  while (queue.length > 0) {
+    const node = queue.shift();
+    nodes.push(node);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+  animateTraversal(nodes);
 }
 
-// Initialize with a tree of multiple nodes
-function initializeTree() {
-    const initialValues = [40, 20, 60, 10, 30, 50];
-    root = new Node(initialValues[0], width / 2, 30); // Start with 40 at center
-    for (let i = 1; i < initialValues.length; i++) {
-        insertNodeHelper(root, initialValues[i], 1);
+function animateTraversal(nodes) {
+  let count = 0;
+  function highlightNext() {
+    if (count < nodes.length) {
+      flickerNode(nodes[count]);
+      count++;
+      setTimeout(highlightNext, 600);
+    } else {
+      alert("Traversal Order: " + nodes.map((n) => n.value).join(" → "));
     }
+  }
+  highlightNext();
+}
+
+function initializeTree() {
+  const initialValues = [40, 20, 60, 10, 30, 50];
+  root = new Node(initialValues[0], width / 2, 30);
+  for (let i = 1; i < initialValues.length; i++) {
+    insertNodeHelper(root, initialValues[i], 1);
+  }
 }
 
 // Initialize the tree
